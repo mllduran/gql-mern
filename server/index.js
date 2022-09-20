3e7fceb2326b3d6f6a8559cc0a0b55ff574ef433
martinlloydduran <martinlloyd.duran@asurion.com>
2022-09-20 17:45:47 +0800

    Initial Commit

---
+++ server/index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const cors = require('cors');
+const schema = require('./schema/schema');
+const connectDB = require('./config/db');
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+connectDB();
+
+app.use(cors());
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: process.env.NODE_ENV === 'development'
+}));
+
+app.listen(port, console.log(`Running on port ${port}`));
